7a772b877782eb13ee4709001eeb730b8a2a1643
shmulikhau <shmuelCEO@gmail.com>
2025-06-16 01:04:22 +0300

    add select_random_kernels function

--- src/image_clusterer/k_means.py
+++ src/image_clusterer/k_means.py
@@ -1,5 +1,18 @@
+import torch
 from src.image_clusterer.clusterer import Clusterer
+
+RANDOM_SCALE: float = 4.
 
 class KMeansClusterer(Clusterer):
     def make_cluster(self, gpu: bool = False):
         return super().make_cluster()
+    
+    def select_random_kernels(self, n_kernels):
+        n_vectors, _ = self.data.shape
+        a = torch.rand(1, n_kernels).to(self.device).pow(RANDOM_SCALE)
+        a[:,0] = a[:,0] * (n_vectors - n_kernels)
+        a[:,0] = a[:,0].floor()
+        for i in range(1, n_kernels):
+            a[:,i] = a[:,i-1] + 1 + (a[:,i] * (n_vectors - a[:,i-1] - n_kernels + i))
+            a[:,i] = a[:,i].floor()
+        return a[0,:]
