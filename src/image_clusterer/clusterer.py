86433c9a048d79507f5f2101bb9aa0a9e825bc85
shmulikhau <shmuelCEO@gmail.com>
2025-06-16 01:58:09 +0300

    k means implemented now, still need to cheking

--- src/image_clusterer/clusterer.py
+++ src/image_clusterer/clusterer.py
@@ -4,7 +4,7 @@ class Clusterer:
     def __init__(self, embedding_dim, gpu: bool = False):
         self.gpu = gpu
         self.data = torch.zeros(0, embedding_dim).to('cuda' if self.gpu else 'cpu')
-        self.clusters = None
+        self.vectors2kernels = None
 
     def push(self, embedding):
         self.push_batch(torch.stack([embedding]).to('cuda' if self.gpu else 'cpu'))
@@ -12,5 +12,5 @@ class Clusterer:
     def push_batch(self, batch_embedding):
         self.data = torch.cat((self.data, batch_embedding), dim=0)
 
-    def make_cluster(self, *args, **kwargs):
+    def make_cluster(self, n_kernels):
         raise NotImplementedError()
--- src/image_clusterer/k_means.py
+++ src/image_clusterer/k_means.py
@@ -1,11 +1,23 @@
 import torch
+import torch.nn.functional as F
+import os
 from src.image_clusterer.clusterer import Clusterer
 
 RANDOM_SCALE: float = 4.
+MAX_CYCLES = int(os.getenv("K_MEANS_MAX_CYCLES", "100"))
+
 
 class KMeansClusterer(Clusterer):
-    def make_cluster(self):
-        return super().make_cluster()
+    def make_cluster(self, n_kernels):
+        i_kernels = self._select_random_kernels(n_kernels)
+        kernels = self.data[i_kernels]
+        for _ in range(MAX_CYCLES):
+            weights = self.data @ kernels.T
+            self.vectors2kernels = weights.argmax(dim=-1)
+            weights = F.one_hot(self.vectors2kernels).to('cuda' if self.gpu else 'cpu')
+            kernels_tmp = (weights[None,...] * self.data[...,None]).sum(dim=-2)
+            if kernels_tmp.equal(kernels):
+                break
     
     def _select_random_kernels(self, n_kernels):
         n_vectors, _ = self.data.shape
