3a3b370a364ad0c7a9307bf41cb8cbf4391e7fe2
shmulikhau <shmuelceo@gmail.com>
2025-06-16 23:58:32 +0300

    jinaclip implementation, still need to check

--- src/image_embedder/embedder.py
+++ src/image_embedder/embedder.py
@@ -1,5 +1,16 @@
 import numpy as np
 
 class ImageEmbedder:
-    def get_embeddings(self, image_batch) -> np.ndarray:
+
+    def __init__(self, gpu: bool = False):
+        self.gpu = gpu
+        self.model = self.load_model()
+
+    def load_model(self):
+        raise NotImplementedError()
+
+    def get_image_embeddings(self, image_batch) -> np.ndarray:
+        raise NotImplementedError()
+    
+    def get_text_embeddings(self, text) -> np.ndarray:
         raise NotImplementedError()
--- src/image_embedder/jinaclip_embedder.py
+++ src/image_embedder/jinaclip_embedder.py
@@ -1,8 +1,21 @@
+from PIL import Image
+import numpy as np
+from transformers import AutoModel
 from src.image_embedder.embedder import ImageEmbedder
 
-class JinaClip(ImageEmbedder):
-    def __init__(self, gpu: bool = False):
-        pass
 
-    def get_embeddings(self, image_batch):
-        return super().get_embeddings(image_batch)
+class JinaClip(ImageEmbedder):
+
+    def load_model(self):
+        return AutoModel.from_pretrained('jinaai/jina-clip-v2', trust_remote_code=True)\
+            .to('cuda' if self.gpu else 'cpu')
+
+    def get_image_embeddings(self, image_batch: list) -> np.ndarray:
+        embeddings = self.model.encode_image(image_batch, batch_size=len(image_batch))
+        embeddings /= np.linalg.norm(embeddings, axis=-1, keepdims=True)
+        return embeddings
+    
+    def get_text_embeddings(self, text) -> np.ndarray:
+        embeddings = self.model.encode_text(text)
+        embeddings /= np.linalg.norm(embeddings, axis=-1, keepdims=True)
+        return embeddings
